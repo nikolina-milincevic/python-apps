3fb9b6dd9d0e215b3aba1ba3b455fe737690cdcb
nikolina-milincevic <nikolina.milincevic@gmail.com>
2024-06-10 22:06:02 +0200

    bug fix and app makind with platypus

--- src/feet_inches_converter.py
+++ src/feet_inches_converter.py
@@ -1,16 +1,19 @@
 import FreeSimpleGUI as sg
 from functions import convert
 
+sg.theme("Black")
+
 feet_text = sg.Text("Enter feet: ")
 feet_input = sg.InputText(tooltip="entering feet", key="feet")
 inches_text = sg.Text("Enter inches: ")
 inches_input = sg.InputText(tooltip="entering inches", key="inches")
 convert_butoon = sg.Button("Convert")
 output_label = sg.Text(key="output")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("Converter", layout=[[feet_text, feet_input], 
                                         [inches_text, inches_input],
-                                        [convert_butoon, output_label]],
+                                        [convert_butoon, exit_button, output_label]],
                    font=("Helvetica", 20))
 while True:
     event, values = window.read()
@@ -19,22 +22,24 @@ while True:
         case "Convert":
             try:
                 if values["feet"] == "":
-                    feet_int = 0
+                    feet_float = 0.0
                     if values["inches"] == "":
-                        inches_int = 0
+                        inches_float = 0.0
                     else:
-                        inches_int = int(values["inches"])
+                        inches_float = float(values["inches"])
                 else:
                     if values["inches"] == "":
-                        feet_int = int(values["feet"])
-                        inches_int = 0
+                        feet_float = float(values["feet"])
+                        inches_float = 0.0
                     else:
-                        feet_int = int(values["feet"])
-                        inches_int = int(values["inches"])
-                meters = convert(feet_int, inches_int)
+                        feet_float = float(values["feet"])
+                        inches_float = float(values["inches"])
+                meters = convert(feet_float, inches_float)
                 window['output'].update(value=f"{meters} m")
             except ValueError:
                 window["output"].update(value="Wrong input. Please try again.")
+        case "Exit":
+            break
         case sg.WIN_CLOSED:
             break
     
