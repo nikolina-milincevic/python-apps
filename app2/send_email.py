c12557e703bed8d06cc5b2521367a8ad30995503
nikolina-milincevic <nikolina.milincevic@gmail.com>
2024-06-17 22:55:25 +0200

    send emails using the form

--- app2/send_email.py
+++ app2/send_email.py
@@ -2,21 +2,19 @@ import smtplib, ssl
 
 # this now does not work because I used prof's
 # username and password which now don't work
+# I also send the mail to some other address 
+# and not to my real address
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-username = "app8flask@gmail.com"
-password = "pmuuyjaxfejfpxkc"
+    username = "app8flask@gmail.com"
+    password = "pmuuyjaxfejfpxkc"
 
-receiver = "app8flask@gmail.com"
-my_context = ssl.create_default_context()
+    receiver = "app8flask@gmail.com"
+    my_context = ssl.create_default_context()
 
-message = """\
-Subject: Hi!
-How are you?
-"""
-
-with smtplib.SMTP_SSL(host, port, context=my_context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
