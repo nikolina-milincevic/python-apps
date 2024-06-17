2979f63329322cf890f8c051d535ce658f494fda
nikolina-milincevic <nikolina.milincevic@gmail.com>
2024-06-17 22:20:03 +0200

    add navigation menu and contact page

--- app2/Home_Page.py
+++ app2/Home_Page.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("./app2/images/photo.png", width=500)
+    st.image("./app2/images/photo.png", width=400)
     
 with col2:
     st.title("Ardit Sulce")
