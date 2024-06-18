25794e1296764b24b358fdb548c05f81af8ab47d
nikolina-milincevic <nikolina.milincevic@gmail.com>
2024-06-18 14:55:52 +0200

    add footer and lines to pdf

--- app3/app3.py
+++ app3/app3.py
@@ -11,9 +11,21 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
-    pdf.line(10, 21, 200, 21)
+    
+    for i in range(20, 280, 10):
+        pdf.line(10, i, 200, i)
+    pdf.ln(246)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=8, txt=row["Topic"], align="R")
     
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        for i in range(31, 270, 10):
+            pdf.line(10, i, 200, i)
+        pdf.ln(250)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=8, txt=row["Topic"], align="R")
 
 pdf.output("./app3/output.pdf")
