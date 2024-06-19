e0dc50b2ac9dc586d14aa94a592f220ed3e2735e
nikolina-milincevic <nikolina.milincevic@gmail.com>
2024-06-19 22:26:43 +0200

    adding multi cell to pdf

--- myproject4/myproject.py
+++ myproject4/myproject.py
@@ -7,9 +7,14 @@ my_files = glob.glob("./myproject4/files/*.txt")
 pdf = FPDF(orientation="P", format="A4", unit="mm")
 
 for filename in my_files:
-    filename = Path(filename).stem.title()
+    my_filename = Path(filename).stem.title()
+    with open(filename, "r") as file:
+        content = file.read()
+        
     pdf.set_font(family="Times", style="B", size=16)
     pdf.add_page()
-    pdf.cell(w=50, h=8, txt=filename, ln=1)
+    pdf.cell(w=50, h=8, txt=my_filename, ln=1)
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=8, txt=content)
     
 pdf.output("./myproject4/output.pdf")
