7f926e966748b2a52a7c7a7c3d078674f1fef28f
nikolina-milincevic <nikolina.milincevic@gmail.com>
2024-07-03 13:43:44 +0200

    practice project commit

--- app6/app6.py
+++ app6/app6.py
@@ -2,17 +2,22 @@ from flask import Flask, render_template
 
 app = Flask("Website")
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
-# here, tutorial.html needs to be in a folder named templates
-# because render_template() looks for this file in such folder
-# but since in VSCode i openned python apps folder, then 
-# my folder templates needs to be in that folder and 
-# not in the folder app6
+    return render_template("home.html")
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
+# execute only if run from this script
+# why - because we may want to use only certain elements
+# from this app in another project and not to run this whole script 
+# as an argument we can add port=5001
+# that is used when we have to apps in order for them not to
+# crash, we execute them on diff ports (default is 5000)
