05c7542376429a50c3bf7e4de5c83c167f506df8
nikolina-milincevic <nikolina.milincevic@gmail.com>
2024-07-05 15:02:26 +0200

    Finishing touches to weather app - warning and temperature

--- app7/app7.py
+++ app7/app7.py
@@ -8,23 +8,25 @@ place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5, 
                  help="Select the number of forecasted days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
-st.subheader(f"{option} for the next {days} days in {place}")
+st.subheader(f"{option} for the next {days} days in {place.capitalize()}")
     
 if place:
-    # Get the data
-    data = get_data(place, days)
+    try:
+        # Get the data
+        data = get_data(place.capitalize(), days)
 
-    if option == "Temperature":
-        temperatures = [dicti["main"]["temp"] / 10 for dicti in data]
-        dates = [dicti["dt_txt"] for dicti in data]
-        # Create a temperature plot
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Dates", "y": "Temperatures (C)"})
-        st.plotly_chart(figure)
-        
-    if option == "Sky":
-        images = {"Clear": "app7/images/clear.png", "Clouds": "app7/images/cloud.png", 
-                  "Rain": "app7/images/rain.png", "Snow": "app7/images/snow.png"}
-        sky_conditions = [dicti["weather"][0]["main"] for dicti in data]
-        image_paths = [images[condition] for condition in sky_conditions]
-        st.image(image_paths, width=115)
-        
+        if option == "Temperature":
+            temperatures = [dicti["main"]["temp"] / 10 for dicti in data]
+            dates = [dicti["dt_txt"] for dicti in data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Dates", "y": "Temperatures (C)"})
+            st.plotly_chart(figure)
+            
+        if option == "Sky":
+            images = {"Clear": "app7/images/clear.png", "Clouds": "app7/images/cloud.png", 
+                    "Rain": "app7/images/rain.png", "Snow": "app7/images/snow.png"}
+            sky_conditions = [dicti["weather"][0]["main"] for dicti in data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.warning("Please write name of the existing place!")  
