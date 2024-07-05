a9c92e9958d09be8dc0210846f31f006987ce995
nikolina-milincevic <nikolina.milincevic@gmail.com>
2024-07-05 14:31:48 +0200

    adding backend to weather data app

--- app7/app7.py
+++ app7/app7.py
@@ -1,21 +1,18 @@
 import streamlit as st
 import plotly.express as px
+from backend_app7 import get_data
 
+# Add the slider and inout boxes
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5, 
                  help="Select the number of forecasted days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
-
-def get_data(days_local):
-    dates = ["2024-07-04", "2024-07-05", "2024-07-06"]
-    temperatures = [7, 10, 11]
-    temperatures = [days_local * i for i in temperatures]
-    # this is just to showcase how dynamic plotting works
-    return dates, temperatures
     
-d, t = get_data(days)
+# Get the data
+d, t = get_data(place, days, option)
 
+# Create a temperature plot
 figure = px.line(x=d, y=t, labels={"x": "Dates", "y": "Temperatures (C)"})
 st.plotly_chart(figure)
---
+++ app7/backend_app7.py
@@ -0,0 +1,27 @@
+# go to openweathermap.org, log in (or create an account),
+# and under API find 5 days/3 hours weather forecast
+# find API call text and copy
+# it is: api.openweathermap.org/data/2.5/forecast?q={cityname}&appid={APIkey}
+# then find your API key in your user account info
+# here I write the API key used in the lection
+
+import requests
+
+API_KEY = "141710af2113bab9f55ef73e1bcd33d5"
+
+def get_data(place_local, days_local=None, option_local=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place_local}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    # the data has values for every 3 hours, that is, 8 values for 1 day
+    nr_values = 8 * days_local
+    filtered_data = filtered_data[:nr_values]
+    if option_local == "Temperature":
+        filtered_data = [dicti["main"]["temp"] for dicti in filtered_data]
+    if option_local == "Sky":
+        filtered_data = [dicti["weather"]["main"] for dicti in filtered_data]
+    return filtered_data
+
+if __name__ == "__main__":
+    print(get_data(place_local="Tokyo", days_local=3, option_local="Temperature"))
