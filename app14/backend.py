1a8a56694f47ffe98063487175e8994ee329cb82
nikolina-milincevic <nikolina.milincevic@gmail.com>
2024-07-26 18:23:34 +0200

    small edit

--- app14/backend.py
+++ app14/backend.py
@@ -5,7 +5,7 @@ import openai
 
 class Chatbot:
     def __init__(self):
-        openai.api_key = "APIKEY"
+        openai.api_key = "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"
         
     def get_response(self, user_input):
         response = openai.Completion.create(
